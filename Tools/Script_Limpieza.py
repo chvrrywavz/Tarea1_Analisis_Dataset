efbce8088d604e1fd4f86c3993b6501b48c5d944
Ignacio Rivera <ignacio.rivera.varela@gmail.com>
2026-04-30 22:53:00 -0400

    Added Skeleton Files

---
+++ Tools/Script_Limpieza.py
@@ -0,0 +1,3 @@
+#el archivo limpio tiene que tener de nombre bitcoin_tweets_last_limpio.csv y salir a /salidas
+
+base = ("source_csv\bitcoin_tweets_latest.csv")
---
+++ Tools/Script_Prepros.py
@@ -0,0 +1,6 @@
+#este script permite generar datos sinteticos debido a los requerimientos del trabajo.
+import pandas as pd
+import random as rad
+
+#analizamos el dataset original.
+leer_archivo = pd.read_csv("source_csv\bitcoin_tweets_latest.csv")
