811d484e0d11d0c8e02a37f763afb4e1bd5908c3
Ignacio Rivera <ignacio.rivera.varela@gmail.com>
2026-05-18 22:17:00 -0400

    Fix for main.py

--- instrucciones.py
+++ instrucciones.py
@@ -1,26 +1,51 @@
-class NodoPost:
-    def __init__(self, id_post, texto):
-        self.id_post = id_post      
-        self.texto = texto         
-        self.likes = []            
-        self.siguiente = None       # Puntero al siguiente nodo (solo python)
+#vamos a usar nlpk https://www.geeksforgeeks.org/nlp/removing-stop-words-nltk-python/ 
+import csv
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+stopwords = set(stopwords.words('english'), set(stopwords.words('spanish')))
+
+
+class nodoPost: 
+    def __init__(self, autor, texto, likes):
+        self.autor = autor
+        self.texto = texto
+        self.likes = likes
+        self.siguiente = None
+
+class NodoContacto:
+    def __init__(self, nombreAmigo):
+        self.nombreAmigo = nombreAmigo
+        self.siguiente = None
+
 class ListaEnlazadaPosts:
     def __init__(self):
-        self.cabeza = None         
-    def insertar(self, id_post, texto):
-        nuevoNodo = NodoPost(id_post, texto)
+        self.cabeza = None
+
+    def insertar(self, autor, texto, likes):
+        nuevoNodo = nodoPost(autor, texto, likes)
         if self.cabeza is None:
             self.cabeza = nuevoNodo
-            return
-        actual = self.cabeza
-        while actual.siguiente is not None:
-            actual = actual.siguiente
-        actual.siguiente = nuevoNodo
-        
-    def agregar_likes_a_post(self, id_post, lista_likes):
-        actual = self.cabeza
-        while actual is not None:
-            if actual.id_post == id_post:
-                actual.likes = lista_likes
-                return
-            actual = actual.siguiente
+        else:
+            actual = self.cabeza
+            while actual.siguiente is not None:
+                actual = actual.siguiente
+            actual.siguiente = nuevoNodo
+
+class ListaEnlazadaContactos:
+    def __init__(self):
+        self.cabeza = None
+
+    def insertar(self, nombre_amigo):
+        nuevoNodo = NodoContacto(nombre_amigo)
+        if self.cabeza is None:
+            self.cabeza = nuevoNodo
+        else:
+            actual = self.cabeza
+            while actual.siguiente is not None:
+                actual = actual.siguiente
+            actual.siguiente = nuevoNodo
